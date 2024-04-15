9ef1e0bab33214bd27ca6c1c7fa684723c9f28d1
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-04-16 01:19:49 +0530

    Initial commit

---
+++ SleepCalculator/src/main/java/com/mycompany/sleepcalculator/MyCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sleepcalculator;
+
+/**
+ *
+ * @author erand
+ */
+public class MyCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    
+    
+}
